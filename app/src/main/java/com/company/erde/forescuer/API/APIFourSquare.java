52126397ab23a7789889e03cc585fb4d9d39c6f3
Erik <erikmartin19@gmail.com>
2017-09-19 17:52:29 -0500

    Initial commit app

---
+++ app/src/main/java/com/company/erde/forescuer/API/APIFourSquare.java
@@ -0,0 +1,10 @@
+package com.company.erde.forescuer.API;
+
+/**
+ * Created by Erik on 18/09/2017.
+ */
+
+public class APIFourSquare {
+    private String user_id = "1ZYUHD2OYE1N5DOLZP1VKXBPVGXRH2TMJBXAUSZ43CQHZ0Y2";
+    private String secret = "TOOPUX43YXE4W5Q2GF2QL0IUDPMIWJJMOWB2RPLWEUS23F3L";
+}
